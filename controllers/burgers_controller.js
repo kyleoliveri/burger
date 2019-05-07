7442285d1a62f9e0868a042a8fd138cb5cefb865
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-05-06 21:19:46 -0700

    adding routes to the controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    cat.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
