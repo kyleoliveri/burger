2b926f153eb492da32c613912e2dec9fda83d97b
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-05-06 20:21:15 -0700

    created config folder, added connection and orm

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kyleoli10",
+    database: "burgers_db"
+});
+
+// Makes the connection to MYSQL
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection for the ORM
+module.exports = connection;
