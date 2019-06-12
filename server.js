db6bd0f6b3b9ecd98a8d22c92eac8d34d7535060
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-06-11 21:10:58 -0700

    updated server

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
+// Dependencies
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
-
-// Serves static contect for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Handlebars
 var exphbs = require("express-handlebars");
 
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them
-var routes = require("./controllers/burgers_controller");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(routes);
+// Requiring our models for syncing
+var db = require("./models");
 
-//Start our server so that it can begin listening to client requests
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost" + PORT);
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
