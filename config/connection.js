a011064f7fdeb119ad42bb7e36f84e1b31b98b42
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-06-11 21:19:07 -0700

    Updated server, connection and gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
     port: 3306,
-    user: "l296lmge3v161nbq",
-    password: "lmwkliiumvdsz0c3",
-    database: "k2j3d6wdl5v22k68"
+    user: "root",
+    password: "root123",
+    database: "burgers_db"
+  });
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-// Makes the connection to MYSQL
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connection: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-// Export the connection for the ORM
+// Export connection for our ORM to use.
 module.exports = connection;
